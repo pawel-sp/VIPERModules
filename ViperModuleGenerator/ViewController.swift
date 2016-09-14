08b2179ad1b66dfdf4f67891ac2ab0c673ea37c8
Paweł Sporysz <pawel.s@untitledkingdom.co>
2016-09-14 10:31:27 +0200

    Added initial project

---
+++ ViperModuleGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ViperModuleGenerator
+//
+//  Created by Paweł Sporysz on 14.09.2016.
+//  Copyright © 2016 Paweł Sporysz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
