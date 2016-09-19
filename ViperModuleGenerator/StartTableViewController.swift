dc5706e6d843de70250274cf490ee82e332b4b8d
Paweł Sporysz <pawel.s@untitledkingdom.co>
2016-09-19 09:46:01 +0200

    Prepared project for example which support nsfetchresultcontroller

---
+++ ViperModuleGenerator/StartTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  StartTableViewController.swift
+//  ViperModuleGenerator
+//
+//  Created by Paweł Sporysz on 19.09.2016.
+//  Copyright © 2016 Paweł Sporysz. All rights reserved.
+//
+
+import UIKit
+
+class StartTableViewController: UITableViewController {
+
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch indexPath.row {
+            case 0:
+                navigationController?.pushViewController(ExampleWireframe().customModule(dataLimit: 5), animated: true)
+            case 1:
+                break
+            default:
+                break
+        }
+    }
+
+}
